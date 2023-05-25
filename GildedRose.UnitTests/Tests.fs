de79e31a74e49b79166ee5c41ccf7de35be5f71a
LucasD1az <110778622+LucasD1az@users.noreply.github.com>
2023-05-24 22:32:39 -0300

    Toqué un poco el gilded rose

--- GildedRose.UnitTests/Tests.fs
+++ GildedRose.UnitTests/Tests.fs
@@ -10,11 +10,11 @@ open GildedRose.UnitTests.Items
 [<Test>]
 let ``My test`` () =
     let Items = new List<Item>()  
-    Items.Add({Name = "foo"; SellIn = 0; Quality = 0})
+    Items.Add({Name = "+5 Dexterity Vest"; SellIn = 10; Quality = 20})
     let app = new GildedRose(Items)
     app.UpdateQuality()
     
-    let expectedItem = {Name = "foo"; SellIn = -1; Quality = 0}
+    let expectedItem = {Name = "+5 Dexterity Vest"; SellIn = 9; Quality = 19}
     
     Assert.AreEqual(expectedItem,Items[0])
     
